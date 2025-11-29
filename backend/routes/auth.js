d1c3cd0ca6721874167eaaafdc4949e0ee6533c5
Samarth Talawar <samarthpt216@gmail.com>
2025-11-29 20:00:25 +0530

    made login endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -49,8 +49,47 @@ router.post('/createuser',[
     } catch (error) {
         // for some other error
         console.log(error.message);
-        res.status(500).send("Some Error occured");
+        res.status(500).send("Server error");
     }
+})
+
+//login - authenticating a user
+
+router.post('/login',[
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists()
+] , async (req, res)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {email, password} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if (!user) {
+            return res.status(400).json({error: "Please enter correct credentials"});
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if(!passwordCompare) {
+            return res.status(400).json({error: "Please enter correct credentials"});
+        }
+
+    const data = {
+            user: {
+                id: user.id
+            }
+        }
+         const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({authtoken});
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Server error");
+    }
+
 })
 
 module.exports = router
