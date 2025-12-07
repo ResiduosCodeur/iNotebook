01aca4144dca07eaa8e448dc12bd9908c1b66c1b
Samarth Talawar <samarthpt216@gmail.com>
2025-12-07 19:12:10 +0530

    started frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Navbar/>
       <h1>Welcome to iNotebook</h1>
-    </div>
+    
+    </>
   );
 }
 
