e58967bea504d62a843dea56182b09a6eff837f7
Samarth Talawar <samarthpt216@gmail.com>
2025-12-15 20:36:30 +0530

    used context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import NoteState from "./context/notes/Notestate.js";  
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
+      <NoteState>
+        <Router>
+          <Navbar />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Router>
+      </NoteState>
     </>
   );
 }
--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useContext, useEffect, useEffectEvent } from "react";
+import noteContext from "../context/notes/noteContext";
 
 const About = () => {
+  const a = useContext(noteContext);
+  useEffect(() => {
+    a.update();
+  }, []);
   return (
     <div>
-      This is About
+      This is About {a.state.name} and he is in class {a.state.class}
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
