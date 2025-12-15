5129b96c3b3657f18f1ea42efb41345f5f551697
Samarth Talawar <samarthpt216@gmail.com>
2025-12-15 17:01:20 +0530

    made navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>This is Home</h2>
+      <h1> Welcome to iNotebook</h1>
+    </div>
+  );
+};
+
+export default Home;
